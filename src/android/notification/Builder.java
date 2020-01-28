5e32a86eaacb371f7c2bcf95c08050bc72af0e21
WGMB <41430264+WGMB@users.noreply.github.com>
2020-01-28 13:29:59 +0200

    Update Builder.java

--- src/android/notification/Builder.java
+++ src/android/notification/Builder.java
@@ -30,7 +30,7 @@ import android.os.Bundle;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationCompat.MessagingStyle.Message;
 import androidx.media.app.NotificationCompat.MediaStyle;
-import androidx.media.MediaBrowserServiceCompat;
+import androidx.media.MediaBrowserServiceCompat.MediaSessionCompat;
 
 import java.util.List;
 import java.util.Random;
